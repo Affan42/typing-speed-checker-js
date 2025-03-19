db78593567e609771d86725056ee7994dc0f6f70
Affan <affandeveloper3424@gmail.com>
2025-03-19 20:41:41 +0530

    on type check incorrect or correct key is typed or not

--- app.js
+++ app.js
@@ -1,40 +1,107 @@
 const elements = {
-  startBtn: document.querySelector('.start'),
-  timeElem: document.querySelector('.time span b'),
-  input:  document.querySelector('.input-field')
+  startBtn: document.querySelector(".start"),
+  timeElem: document.querySelector(".time span b"),
+  input: document.querySelector(".input-field"),
+  text: document.querySelector(".typing-text p"),
+};
+// Game State
+
+const state = {
+  lettersArr: [],
+  wordPosition: 0,
+};
+
+// Set Texts
+
+function setText() {
+  let readyText = "";
+
+  state.lettersArr.map((letter) => (readyText += letter));
+
+  elements.text.innerHTML = readyText;
 }
+
+// Set Texts Arr
+
+function setTextsArr() {
+  const text = fetchText();
+
+  state.lettersArr = Array.from(text);
+}
+
 // Fetch Text
 
+function fetchText() {
+  return "lorem ipsum";
+}
 // Timer
-function startTimer () {
-  let sec = 60
- let countDown =  setInterval(() => {
-    sec--
-    elements.timeElem.innerHTML = sec
+
+function startTimer() {
+  let sec = 60;
+  let countDown = setInterval(() => {
+    sec--;
+    elements.timeElem.innerHTML = sec;
+
+    if (sec <= 0) {
+      clearInterval(countDown);
+    }
+  }, 1000);
+}
+
+function forword() {
+  let currentLetter = state.lettersArr[state.wordPosition];
+
+  state.lettersArr[
+    state.wordPosition
+  ] = `<span class="active" >${currentLetter}</span>`;
+
+  setText();
+
+  state.lettersArr[state.wordPosition] = currentLetter;
   
-  
-    if(sec <= 0){
-    clearInterval(countDown)
+  state.wordPosition++;
+}
+
+function letterCheck(keyPressed) {
+  let currentLetter = state.lettersArr[state.wordPosition - 1];
+
+  if (currentLetter === keyPressed) {
+    state.lettersArr[
+      state.wordPosition - 1
+    ] = `<span class="correct" >${currentLetter}</span>`;
+  } else {
+    state.lettersArr[
+      state.wordPosition - 1
+    ] = `<span class="incorrect" >${currentLetter}</span>`;
   }
-  
-  }, 1000)
+
+  setText();
 }
 // Event Listeners
-elements.startBtn.addEventListener('click', () => {
-  startTimer()
+elements.startBtn.addEventListener("click", () => {
+  startTimer();
+});
 
-})
+elements.input.addEventListener("keydown", (e) => {
+  console.log(e.key);
 
-elements.input.addEventListener('keydown', () => {
-// fetchRandomText()
-})
-fetchRandomText()
-function fetchRandomText(){
-let text = "lorem ipsum"
-let wordPosition = 0
-const lettersArr = Array.from(text)
-
-
-
-}
-
+  if (
+    e.key === "Backspace" ||
+    e.key === "CapsLock" ||
+    e.key === "ArrowLeft" ||
+    e.key === "ArrowRight" ||
+    e.key === "ArrowUp" ||
+    e.key === "ArrowDown" ||
+    e.key === "Shift" ||
+    e.key === "Delete" 
+  )
+    return;
+  letterCheck(e.key);
+  forword();
+});
+// On app start
+document.addEventListener("DOMContentLoaded", () => {
+  setTextsArr();
+  setText();
+  forword();
+});
